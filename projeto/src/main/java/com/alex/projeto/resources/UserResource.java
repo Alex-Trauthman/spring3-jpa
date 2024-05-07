43e3e3c3cb0b15e59e8c20734e58e6d170b09bc4
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-06 22:02:26 -0300

    added user and userResource

---
+++ projeto/src/main/java/com/alex/projeto/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.alex.projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alex.projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public  ResponseEntity<User> findAll(){
+		User u = new User(1L,"cleito","cleito@gmail.com","412341","senhadocleito");
+		return ResponseEntity.ok().body(u);
+	}
+}
