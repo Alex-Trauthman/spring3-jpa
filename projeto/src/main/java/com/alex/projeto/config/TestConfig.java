744bf55cbfa9a5f8b04e0504d0cacf3f6665069a
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-09 22:31:43 -0300

    done payment

--- projeto/src/main/java/com/alex/projeto/config/TestConfig.java
+++ projeto/src/main/java/com/alex/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.alex.projeto.entities.Category;
 import com.alex.projeto.entities.Order;
 import com.alex.projeto.entities.OrderItem;
+import com.alex.projeto.entities.Payment;
 import com.alex.projeto.entities.Product;
 import com.alex.projeto.entities.User;
 import com.alex.projeto.entities.enums.OrderStatus;
@@ -76,6 +77,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T20:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 	
