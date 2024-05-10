87df875b6d74253d4e19d799e68d0d50a9476a8a
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-09 21:50:29 -0300

    starting to add orderItem

---
+++ projeto/src/main/java/com/alex/projeto/entities/OrderItem.java
@@ -0,0 +1,77 @@
+package com.alex.projeto.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.alex.projeto.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_ordem_item")
+public class OrderItem implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@EmbeddedId
+	private OrderItemPK id;
+	private int quantity;
+	private Double price;
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public OrderItem(Order order, Product product, int quantity, Double price) {
+		super();
+		this.id.setOrder(order);
+		this.id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public OrderItem() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
