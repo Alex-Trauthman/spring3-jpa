4ffcfceb1a75f7bd54cfc7d3b73a99d1ec647afe
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-09 21:25:15 -0300

    proper handled many to many

--- projeto/src/main/java/com/alex/projeto/entities/Product.java
+++ projeto/src/main/java/com/alex/projeto/entities/Product.java
@@ -9,8 +9,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
@@ -18,7 +21,7 @@ public class Product implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,9 +29,11 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
-	private Set<Category> categories = new HashSet<>();
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id")
+	, inverseJoinColumns = @JoinColumn(name = "category_id"))
+	Set<Category> categories = new HashSet<>();
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
@@ -46,6 +51,7 @@ public class Product implements Serializable {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -53,6 +59,7 @@ public class Product implements Serializable {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -60,6 +67,7 @@ public class Product implements Serializable {
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
@@ -67,6 +75,7 @@ public class Product implements Serializable {
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
@@ -74,6 +83,7 @@ public class Product implements Serializable {
 	public String getImgUrl() {
 		return imgUrl;
 	}
+
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
@@ -86,6 +96,7 @@ public class Product implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -98,7 +109,4 @@ public class Product implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
-	
 }
