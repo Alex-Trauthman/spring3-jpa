aeaa75abb4624081ca92f5b6dc44363f53e8e9a8
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-05-10 10:52:57 -0300

    proper handled update 404 exception

--- projeto/src/main/java/com/alex/projeto/services/UserService.java
+++ projeto/src/main/java/com/alex/projeto/services/UserService.java
@@ -11,6 +11,8 @@ import com.alex.projeto.entities.User;
 import com.alex.projeto.repositories.UserRepository;
 import com.alex.projeto.services.exceptions.DatabaseException;
 import com.alex.projeto.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 @Service
 public class UserService {
 	@Autowired
@@ -38,9 +40,13 @@ public class UserService {
 	    }	
 	}
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
